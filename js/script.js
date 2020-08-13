d71916080a5fd613423fa5837f51d585c8d7c8fe
Allen J Price <67703616+AllenJPrice283@users.noreply.github.com>
2020-08-13 21:06:39 +0100

    Updated script.js
    Removed original comments and added core varibles and functions.

--- js/script.js
+++ js/script.js
@@ -5,46 +5,31 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+/**
+ * This "List Filter and Pagination" application takes a list of students, tallies up the total and
+ * displays a maximum of ten students per page.
+ * A new page is created every time the limit of tenstudents is reached.
+ * Students can also be filtered by search.
+ **/
 
 
+// The parent node to append the pagination links to
+const mainPage = document.querySelector('.page');
+
+// The list of students to filter out and paginate
+const studentItems = document.querySelectorAll('.student-item');
+
+// The parent node to append the student search bar to
+const pageHeader = document.querySelector('.page-header');
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+// showPage function
+const showPage = (list, page) => {};
 
 
+// appendPageLinks function
+const appendPageLinks = list => {};
 
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// appendStudentSearch function
+const appendStudentSearch = list => {};
