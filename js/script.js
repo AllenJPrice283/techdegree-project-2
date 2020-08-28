8f591fd9c926319fa4fb6b3beebc9a5fa9ab412b
Allen J Price <67703616+AllenJPrice283@users.noreply.github.com>
2020-08-29 00:35:51 +0100

    Updated script.js
    Tested the getPageList function. Created getPageLinks and appendPageLinks functions. getPageLinks() acts as a helper function for appendPageLinks().

--- js/script.js
+++ js/script.js
@@ -97,8 +97,6 @@ const getPageList = (list, page) => {
    return pageList;
 };
 
-console.log(getPageList(originalListItems, 1));
-
 /**
  * Display the trimmed list of at least ten students per page
  * @param {NodeListOf<HTMLLIElement} list HTML LI elements of students to display
@@ -108,4 +106,54 @@ const showPage = (list, page) => {
    let newPageList = getPageList(list, page);
    let oldPageList = parentPage.querySelector('.student-list');
    parentPage.replaceChild(newPageList, oldPageList);
+};
+
+// for testing - delete after next save
+showPage(originalListItems, 1);
+
+/**
+ * Dynamically-created UL element containing links acting as page numbers
+ * @param {NodeListOf<HTMLLIElement>} list HTML LI elements of students to display
+ * @returns {HTMLUListElement} HTML UL element containing links acting as page numbers
+ */
+const getPageLinks = list => {
+   const numberOfPages = Math.ceil(list.length / maxListItems);
+
+   let pagesList = document.createElement('ul');
+
+   for (let i = 1; i <= numberOfPages; i++) {
+      let pagesListItem = document.createElement('li');
+      let pageLink = document.createElement('a');
+      pageLink.href = "#";
+      pageLink.innerHTML = i;
+
+      pagesListItem.appendChild(pageLink);
+      pagesList.appendChild(pagesListItem);
+   }
+
+   let activeDefault = pagesList.getElementsByTagName('a')[0];
+   activeDefault.className = "active";
+
+   pagesList.addEventListener('click', event =>{
+      let pageLinks = pagesList.getElementsByTagName('a');
+      for (let i = 0; i < pageLinks.length; i++) {
+         pageLinks[i].className = "";
+      }
+
+      if (event.target.tagName === 'A') {
+         let activeLink = event.target;
+         activeLink.className = "active";
+         showPage(list, event.target.innerHTML);
+      }
+   });
+   return pagesList;
+};
+
+/**
+ * Display the page numbers by appending the page links to the Pagination Div element
+ * @param {NodeListOf<HTMLLIElement>} list HTML LI elements of students to display
+ */
+const appendPageLinks = list => {
+   showPage(list, 1);
+   pagination.appendChild(getPageLinks(list));
 };
